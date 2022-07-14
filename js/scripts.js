5714261898e46a44a5a2f6946621daebd2e93851
BakaminovD <dbbakaminov@gmail.com>
2022-07-14 23:22:33 +0300

    Слайдер фото, кнопки слайдера, основной шрифт Убунту

--- js/scripts.js
+++ js/scripts.js
@@ -41,27 +41,51 @@ function scrollTopTop(){
 };
 
 /* script for photo-popup and close-button */
+let photos = [
+    'person/Aleksandra_Zubets.PNG', 
+    'person/Anastasiya_Mishkina.PNG',
+    'person/Daria_Isakova.PNG',
+    'person/Daria_Petrova.PNG',
+    'person/Dmitrii_Atlas.PNG',
+    'person/Ekaterina_Markova.PNG',
+    'person/Iulia_efimova.PNG',
+    'person/Olesia_Dukar.PNG', 
+    'person/Sergey_Zarechenskii.PNG', 
+    'person/Veniamin_Bashkov.jpg' 
+  ];
 
 function getId(event){
-    let photos = [
-      'person/Aleksandra_Zubets.PNG', 
-      'person/Anastasiya_Mishkina.PNG',
-      'person/Daria_Isakova.PNG',
-      'person/Daria_Petrova.PNG',
-      'person/Dmitrii_Atlas.PNG',
-      'person/Ekaterina_Markova.PNG',
-      'person/Iulia_efimova.PNG',
-      'person/Olesia_Dukar.PNG', 
-      'person/Sergey_Zarechenskii.PNG', 
-      'person/Veniamin_Bashkov.jpg' 
-    ];
     let my_img = document.getElementById('big_img');
     let now_photo = event.target.id;
     document.getElementById('form').style.display = 'flex';
-    my_img.outerHTML = '<img src="' + photos[now_photo] + '" alt="" class="big_img" id="big_img">';
+    my_img.outerHTML = '<img src="' + photos[now_photo] + '" alt="" class="big_img" id="big_img" name="'+ now_photo +'">';
   };
+
+function left_step(){
+    let my_img = document.getElementById("big_img");
+    let now_photo = my_img.name;
+    if (now_photo == 0){
+        now_photo = 9;
+        my_img.outerHTML = '<img src="' + photos[now_photo] + '" alt="" class="big_img" id="big_img" name="'+ now_photo +'">';
+    } else {
+        now_photo = now_photo - 1;
+        my_img.outerHTML = '<img src="' + photos[now_photo] + '" alt="" class="big_img" id="big_img" name="'+ now_photo +'">';
+    };
+};
+
+function right_step(){
+    let my_img = document.getElementById("big_img");
+    let now_photo = my_img.name;
+    if (now_photo == 9){
+        now_photo = 0;
+        my_img.outerHTML = '<img src="' + photos[now_photo] + '" alt="" class="big_img" id="big_img" name="'+ now_photo +'">';
+    } else {
+        now_photo++;
+        my_img.outerHTML = '<img src="' + photos[now_photo] + '" alt="" class="big_img" id="big_img" name="'+ now_photo +'">';
+    };
+};
   
-  function clouse_photo(){
+function clouse_photo(){
     document.getElementById('form').style.display = 'none';
   };
 
